96f65a84284c3fc0ee1df745740a36ab99a09d07
lilin <lilin@ubuntu>
2024-02-26 15:21:00 +0800

    在和后端对结果过程中做了一些小的修改

--- ukb_star_mapping_wrapper.py
+++ ukb_star_mapping_wrapper.py
@@ -4,6 +4,7 @@ import argparse
 import logging
 import os
 import json
+import sys
 import time
 
 def send_json_message(analysis_path:str, send_message_script: str, message: dict, step_file_name:str) -> None:
@@ -22,7 +23,7 @@ def steward(config_file_path:str, ukb_star_mapping_path:str, send_message_script
         'star_index' : config_d['ukbParams']['star_index'],
         'fastq1'     : config_d['ukbParams']['fastq1'],
         'fastq2'     : config_d['ukbParams']['fastq2'],
-        'threads_num': config_d['ukbParams']['threads_num']
+        # 'threads_num': config_d['ukbParams']['threads_num']
     }
     params_file_path = '{}/params.json'.format(analysis_path)
     with open(params_file_path, 'w') as params_f:
@@ -31,12 +32,12 @@ def steward(config_file_path:str, ukb_star_mapping_path:str, send_message_script
     return_value = os.system(ukb_star_mapping_command)
     logging.info(ukb_star_mapping_command)
     logging.info('return value:{}\n'.format(str(return_value)))
-    time.sleep(5)
+    time.sleep(20)
     # send the result files 
     feedback_dict = {
         'uuid'          : config_d['uuid'],
         'ukbId'         : config_d['ukbId'],
-        'ukbToolsCode'  : config_d['ukbToolsCode'],
+        'ukbToolsCode'  : config_d['ukbToolCode'],
         'ukbToolName'   : config_d['ukbToolName'],
         'pipeline'      : 'ukb',
         'analysisStatus': '',
@@ -55,6 +56,7 @@ def steward(config_file_path:str, ukb_star_mapping_path:str, send_message_script
         feedback_dict['endDate']        = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
         feedback_dict['error']          = 1
         send_json_message(analysis_path, send_message_script, feedback_dict, 'start.json')
+        sys.exit()
     flag = 0
     while True:
         execution_trace_file = '{}/results'.format(analysis_path)
